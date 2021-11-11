9f6ec73baec5318188a71c1bc745c1e17dd2d1e9
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-11-11 17:22:06 +0200

    SplashScreen done.

---
+++ app/src/main/java/com/example/easyplan/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.easyplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 6000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.splash_layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(1000);
+        animationDrawable.setExitFadeDuration(2000);
+        animationDrawable.start();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+}
