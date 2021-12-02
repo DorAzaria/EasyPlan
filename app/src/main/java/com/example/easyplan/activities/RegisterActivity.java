d7aa1f6d6b3b1aa757a3c6e9441f72cf3e9caf19
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-12-02 12:23:19 +0200

    firebase

--- app/src/main/java/com/example/easyplan/activities/RegisterActivity.java
+++ app/src/main/java/com/example/easyplan/activities/RegisterActivity.java
@@ -1,24 +1,34 @@
 package com.example.easyplan.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.easyplan.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private Button register_btn;
     private EditText register_username, register_password, register_re_password, register_email;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
 
         register_btn = (Button) findViewById(R.id.register_btn);
         register_username = (EditText) findViewById(R.id.register_username);
@@ -26,14 +36,19 @@ public class RegisterActivity extends AppCompatActivity {
         register_re_password = (EditText) findViewById(R.id.register_re_password);
         register_email = (EditText) findViewById(R.id.register_email);
 
-
-        register_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(RegisterActivity.this, RegisterByTypeActivity.class));
-            }
-        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        updateUI(currentUser);
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+
+        }
+    }
+
 
     private void checkEmpty() {
 
@@ -41,5 +56,37 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void checkInputs() {
 
+    }
+
+    public void register(View view) {
+        register_btn = (Button) view;
+
+
+
+        if(register_password.getText().toString().equals(register_re_password.getText().toString())) {
+
+            mAuth.createUserWithEmailAndPassword(register_email.getText().toString(), register_password.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                FirebaseUser user = mAuth.getCurrentUser();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+            startActivity(new Intent(RegisterActivity.this, RegisterByTypeActivity.class));
+
+        }
+        else {
+            Toast.makeText(RegisterActivity.this, "Passwords doesn't matched", Toast.LENGTH_SHORT).show();
+        }
     }
 }
