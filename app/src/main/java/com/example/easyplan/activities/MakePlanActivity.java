6cc9b91bb4e03f880e888d9beee6d7cebd327f09
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-12-09 17:21:10 +0200

    Dialog Make Plan

--- app/src/main/java/com/example/easyplan/activities/MakePlanActivity.java
+++ app/src/main/java/com/example/easyplan/activities/MakePlanActivity.java
@@ -2,9 +2,12 @@ package com.example.easyplan.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -16,6 +19,8 @@ import com.example.easyplan.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 import java.util.HashMap;
 import java.util.Vector;
@@ -69,8 +74,89 @@ public class MakePlanActivity extends AppCompatActivity {
     }
 
     public void make_plan(View view) {
-        String trainer_id = mAuth.getUid();
-        String trainee_id; /// need to get this id by intent putExtra from TraineeListActivity
+
+        String check = checkInputs();
+        if(check.isEmpty()) {
+            String trainer_id = mAuth.getUid();
+            String trainee_id; /// need to get this id by intent putExtra from TraineeListActivity
+
+            String train_time1 = make_plan_train_time1.getText().toString();
+            String train_exer1 = make_plan_train_exer1.getText().toString();
+
+            String train_time2 = make_plan_train_time2.getText().toString();
+            String train_exer2 = make_plan_train_exer2.getText().toString();
+
+            String train_time3 = make_plan_train_time3.getText().toString();
+            String train_exer3 = make_plan_train_exer3.getText().toString();
+
+            String day1 = make_plan_day_1.getText().toString();
+            String day2 = make_plan_day_2.getText().toString();
+            String day3 = make_plan_day_3.getText().toString();
+            String day4 = make_plan_day_4.getText().toString();
+            String day5 = make_plan_day_5.getText().toString();
+            String day6 = make_plan_day_6.getText().toString();
+            String cheat = make_plan_cheat_day.getText().toString();
+
+            ///////////////////////////////////////////////////////////
+            HashMap<String, HashMap<String, String>> trains = new HashMap<>();
+
+            trains.put("1", new HashMap<>());
+            trains.get("1").put("time", train_time1);
+            trains.get("1").put("exercise", train_exer1);
+
+            trains.put("2", new HashMap<>());
+            trains.get("2").put("time", train_time2);
+            trains.get("2").put("exercise", train_exer2);
+
+            trains.put("3", new HashMap<>());
+            trains.get("3").put("time", train_time3);
+            trains.get("3").put("exercise", train_exer3);
+
+            /////////////////////////////////////////////////////////////
+            Vector<String> menu = new Vector<>();
+            menu.add(day1);
+            menu.add(day2);
+            menu.add(day3);
+            menu.add(day4);
+            menu.add(day5);
+            menu.add(day6);
+            menu.add(cheat);
+
+            Plan plan = new Plan(trainer_id, "Ok7J0x3grFYwqZBzJzGsYqmU1LI3", trains, menu);
+
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference reference = database.getReference("Plans/" + "Ok7J0x3grFYwqZBzJzGsYqmU1LI3");
+            reference.setValue(plan);
+
+            reference = database.getReference("Users/" + mAuth.getUid() + "/my_trainees" + "/Ok7J0x3grFYwqZBzJzGsYqmU1LI3");
+            reference.setValue("true");
+
+            startActivity(new Intent(MakePlanActivity.this, TraineeHomepageActivity.class));
+        }
+        else {
+            final Dialog dialog = new Dialog(MakePlanActivity.this);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(true);
+            dialog.setContentView(R.layout.dialog_make_plan_error);
+
+            TextView errors = dialog.findViewById(R.id.dialog_make_plan_errors);
+            Button ok_btn = dialog.findViewById(R.id.dialog_make_plan_ok);
+
+            errors.setText(check);
+            ok_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+
+            dialog.show();
+        }
+    }
+
+    private String checkInputs() {
+        String errors = "";
 
         String train_time1 = make_plan_train_time1.getText().toString();
         String train_exer1 = make_plan_train_exer1.getText().toString();
@@ -81,22 +167,6 @@ public class MakePlanActivity extends AppCompatActivity {
         String train_time3 = make_plan_train_time3.getText().toString();
         String train_exer3 = make_plan_train_exer3.getText().toString();
 
-        HashMap<String, HashMap<String,String>> trains = new HashMap<>();
-
-        trains.put("1", new HashMap<>());
-        trains.get("1").put("time", train_time1);
-        trains.get("1").put("exercise", train_exer1);
-
-        trains.put("2", new HashMap<>());
-        trains.get("2").put("time", train_time2);
-        trains.get("2").put("exercise", train_exer2);
-
-        trains.put("3", new HashMap<>());
-        trains.get("3").put("time", train_time3);
-        trains.get("3").put("exercise", train_exer3);
-
-        Vector<String> menu = new Vector<>();
-
         String day1 = make_plan_day_1.getText().toString();
         String day2 = make_plan_day_2.getText().toString();
         String day3 = make_plan_day_3.getText().toString();
@@ -105,23 +175,71 @@ public class MakePlanActivity extends AppCompatActivity {
         String day6 = make_plan_day_6.getText().toString();
         String cheat = make_plan_cheat_day.getText().toString();
 
-        menu.add(day1);
-        menu.add(day2);
-        menu.add(day3);
-        menu.add(day4);
-        menu.add(day5);
-        menu.add(day6);
-        menu.add(cheat);
+        if(TextUtils.isEmpty(train_time1)) {
+            make_plan_train_time1.setError("Please insert time.");
+            errors += "Insert 1st train time \n";
+        }
 
-        Plan plan = new Plan(trainer_id, "Ok7J0x3grFYwqZBzJzGsYqmU1LI3", trains, menu);
+        if(TextUtils.isEmpty(train_exer1)) {
+            make_plan_train_exer1.setError("Please insert exercise.");
+            errors += "Insert 1st train exercise \n";
+        }
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference reference = database.getReference("Plans/" + "Ok7J0x3grFYwqZBzJzGsYqmU1LI3");
-        reference.setValue(plan);
+        if(TextUtils.isEmpty(train_time2)) {
+            make_plan_train_time2.setError("Please insert time.");
+            errors += "Insert 2nd train time \n";
+        }
 
-        reference = database.getReference("Users/" + mAuth.getUid() + "/my_trainees"+"/Ok7J0x3grFYwqZBzJzGsYqmU1LI3");
-        reference.setValue("true");
+        if(TextUtils.isEmpty(train_exer2)) {
+            make_plan_train_exer2.setError("Please insert exercise.");
+            errors += "Insert 2nd train exercise \n";
+        }
 
-        startActivity(new Intent(MakePlanActivity.this, TraineeHomepageActivity.class));
+        if(TextUtils.isEmpty(train_time3)) {
+            make_plan_train_time3.setError("Please insert time.");
+            errors += "Insert 3rd train time \n";
+        }
+
+        if(TextUtils.isEmpty(train_exer3)) {
+            make_plan_train_exer3.setError("Please insert exercise.");
+            errors += "Insert 3rd train exercise \n";
+        }
+
+        if(TextUtils.isEmpty(day1)) {
+            make_plan_day_1.setError("Please insert a nutrition.");
+            errors += "Insert day 1 nutrition \n";
+        }
+
+        if(TextUtils.isEmpty(day2)) {
+            make_plan_day_2.setError("Please insert a nutrition.");
+            errors += "Insert day 2 nutrition \n";
+        }
+
+        if(TextUtils.isEmpty(day3)) {
+            make_plan_day_3.setError("Please insert a nutrition.");
+            errors += "Insert day 3 nutrition \n";
+        }
+
+        if(TextUtils.isEmpty(day4)) {
+            make_plan_day_4.setError("Please insert a nutrition.");
+            errors += "Insert day 4 nutrition \n";
+        }
+
+        if(TextUtils.isEmpty(day5)) {
+            make_plan_day_5.setError("Please insert a nutrition.");
+            errors += "Insert day 5 nutrition \n";
+        }
+
+        if(TextUtils.isEmpty(day6)) {
+            make_plan_day_6.setError("Please insert a nutrition.");
+            errors += "Insert day 6 nutrition \n";
+        }
+
+        if(TextUtils.isEmpty(cheat)) {
+            make_plan_day_1.setError("Please insert a nutrition.");
+            errors += "Insert cheat day nutrition \n";
+        }
+
+        return errors;
     }
 }
