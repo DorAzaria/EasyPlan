02cfbc87d152e52564ff298aae37535f7f24a496
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-12-09 18:52:03 +0200

    Inputs check for trainer activity

--- app/src/main/java/com/example/easyplan/activities/RegisterTrainerActivity.java
+++ app/src/main/java/com/example/easyplan/activities/RegisterTrainerActivity.java
@@ -3,15 +3,18 @@ package com.example.easyplan.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.easyplan.Data.Trainee;
@@ -65,54 +68,76 @@ public class RegisterTrainerActivity extends AppCompatActivity {
     }
 
     public void register_trainer(View view) {
+        String check = checkInputs();
+        if(check.isEmpty()) {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                String name = register_trainer_full_name.getText().toString();
+                                String address = register_trainer_address.getText().toString();
+                                int age = Integer.parseInt(register_trainer_age.getText().toString());
+                                int cost = Integer.parseInt(register_trainer_cost.getText().toString());
+                                String education = register_trainer_education.getText().toString();
+                                String personal_page = register_trainer_personal_page.getText().toString();
+                                Vector<String> targets = new Vector<>();
+                                if (register_trainer_cardio.isChecked()) {
+                                    targets.add("Cardio");
+                                }
+                                if (register_trainer_fitness.isChecked()) {
+                                    targets.add("Fitness");
+                                }
+                                if (register_trainer_muscle.isChecked()) {
+                                    targets.add("Muscle");
+                                }
+                                if (register_trainer_menu.isChecked()) {
+                                    targets.add("Menu Nutrition");
+                                }
+                                String gender;
+                                if (register_trainer_female_radio.isChecked()) {
+                                    gender = "female";
+                                } else gender = "male";
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference reference = database.getReference("Users/" + mAuth.getUid());
+                                Trainer trainer = new Trainer(name, address, gender, education, personal_page, age, cost, targets);
+                                reference.setValue(trainer);
+                                Intent move = new Intent(RegisterTrainerActivity.this, TrainerHomepageActivity.class);
+                                startActivity(move);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(RegisterTrainerActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
 
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            String name = register_trainer_full_name.getText().toString();
-                            String address = register_trainer_address.getText().toString();
-                            int age =  Integer.parseInt(register_trainer_age.getText().toString());
-                            int cost = Integer.parseInt(register_trainer_cost.getText().toString());
-                            String education = register_trainer_education.getText().toString();
-                            String personal_page = register_trainer_personal_page.getText().toString();
-                            Vector<String> targets = new Vector<>();
-                            if(register_trainer_muscle.isChecked()){
-                                targets.add("Cardio");
                             }
-                            if(register_trainer_muscle.isChecked()){
-                                targets.add("Fitness");
-                            }
-                            if(register_trainer_muscle.isChecked()){
-                                targets.add("Muscle");
-                            }
-                            if(register_trainer_muscle.isChecked()){
-                                targets.add("Menu Nutrition");
-                            }
-                            String gender;
-                            if (register_trainer_female_radio.isChecked()) {
-                                gender = "female";
-                            } else gender = "male";
-                            FirebaseDatabase database = FirebaseDatabase.getInstance();
-                            DatabaseReference reference = database.getReference("Users/" + mAuth.getUid());
-                            Trainer trainer =  new Trainer(name,address,gender,education,personal_page,age,cost,targets);
-                            reference.setValue(trainer);
-                            Intent move = new Intent(RegisterTrainerActivity.this , TrainerHomepageActivity.class);
-                            startActivity(move);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(RegisterTrainerActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
                         }
-                    }
-                });
+                    });
+        }
+        else {
+            final Dialog dialog = new Dialog(RegisterTrainerActivity.this);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(true);
+            dialog.setContentView(R.layout.dialog_error);
+
+            TextView errors = dialog.findViewById(R.id.dialog_error_text);
+            Button ok_btn = dialog.findViewById(R.id.dialog_ok);
+
+            errors.setText(check);
+            ok_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+
+            dialog.show();
+        }
     }
 
 
-    private String checkInputs() {
+    public String checkInputs() {
         String errors = "";
 
         String name = register_trainer_full_name.getText().toString();
@@ -127,9 +152,19 @@ public class RegisterTrainerActivity extends AppCompatActivity {
             errors += "Insert your name \n";
         }
 
+        if(name.length() > 18) {
+            register_trainer_full_name.setError("Your name is too long (max 18 letters)");
+            errors += "Your name is too long (max 18 letters) \n";
+        }
+
         if(TextUtils.isEmpty(address)) {
             register_trainer_address.setError("Please insert your address.");
             errors += "Insert your address \n";
+        }
+
+        if(address.length() > 25) {
+            register_trainer_full_name.setError("Your address is too long (max 25 letters)");
+            errors += "Your address is too long (max 25 letters) \n";
         }
 
         if(TextUtils.isEmpty(age)) {
@@ -142,12 +177,12 @@ public class RegisterTrainerActivity extends AppCompatActivity {
             errors += "Insert your cost \n";
         }
 
-        if(Integer.parseInt(age) > 120) {
+        if(!TextUtils.isEmpty(age) && Integer.parseInt(age) > 120) {
             register_trainer_age.setError("Age over 120 isn't allowed.");
             errors += "Age over 120 isn't allowed. \n";
         }
 
-        if(Integer.parseInt(cost) > 9000) {
+        if(!TextUtils.isEmpty(cost) && Integer.parseInt(cost) > 9000) {
             register_trainer_age.setError("Cost over 9000 isn't allowed.");
             errors += "Cost over 9000 isn't allowed. \n";
         }
@@ -162,22 +197,17 @@ public class RegisterTrainerActivity extends AppCompatActivity {
             errors += "Insert your personal page \n";
         }
 
-        if(TextUtils.isEmpty(personal_page)) {
-            register_trainer_personal_page.setError("Please insert your personal page.");
-            errors += "Insert your personal page \n";
-        }
-
         int check_multichecks = 0;
-        if(register_trainer_muscle.isChecked()){
+        if (register_trainer_cardio.isChecked()) {
             check_multichecks++;
         }
-        if(register_trainer_muscle.isChecked()){
+        if (register_trainer_fitness.isChecked()) {
             check_multichecks++;
         }
-        if(register_trainer_muscle.isChecked()){
+        if (register_trainer_muscle.isChecked()) {
             check_multichecks++;
         }
-        if(register_trainer_muscle.isChecked()){
+        if (register_trainer_menu.isChecked()) {
             check_multichecks++;
         }
 
